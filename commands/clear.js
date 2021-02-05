2c40310d129448e501657f5f85c9f6b1df2ec7d1
Jithvan <jithvan@bu.edu>
2021-02-05 17:51:42 -0500

    Update clear.js
    This is annoying

--- commands/clear.js
+++ commands/clear.js
@@ -27,11 +27,12 @@ module.exports.run = async (client, message, args) => {
         deleteAmount = parseInt(args[0]);
     }
 
-    message.channel.bulkDelete(deleteAmount, true)
-        .then(deleted => message.channel.send(`Deleted \`${deleted.size}\` messages.`).delete(5000))
+    message.channel.bulkDelete(deleteAmount, true).then(() => {
+        message.channel.send(`Deleted \`` + toString(deleteAmount) + `\` messages.`).then(m => m.delete(3000))
+        })
         .catch(err => message.reply(`Something went wrong... ${err}`));
 }
-
+    
 module.exports.config = {
     name: "clear",
     description: "clears message",

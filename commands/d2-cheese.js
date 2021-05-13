417d92415d9c9bb656c6d175ab8ffd3c781e4fed
Jithvan <jithvan@bu.edu>
2021-05-13 18:31:37 -0400

    Update d2-cheese.js

--- commands/d2-cheese.js
+++ commands/d2-cheese.js
@@ -8,32 +8,85 @@ module.exports.run = async (client, message, args) => {
 
     //Custom Help command by using the second argument.
     if (message.channel.id === channelID) {
-    if(helpArgs[0] === '101') {
-        const embed = new Discord.MessageEmbed()
-        .setAuthor('Cheesing in Destiny 2')
-        .setDescription('The YouTuber [Cheese Forever](https://www.youtube.com/c/CheeseForever/) is the go to place to learn about the latest cheeses in Destiny 2.')
-        .setColor('#ffcc00')
-        .setThumbnail('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/Cheese_Icon.png')
-        .setImage('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/Cheesing101.png')
-        .setTimestamp()
-        message.channel.send(embed);
-    }}
+        if(helpArgs[0] === '101') {
+            const embed = new Discord.MessageEmbed()
+            .setAuthor('Cheesing in Destiny 2')
+            .setDescription('The YouTuber [Cheese Forever](https://www.youtube.com/c/CheeseForever/) is the go to place to learn about the latest cheeses in Destiny 2.')
+            .setColor('#ffcc00')
+            .setThumbnail('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/Cheese_Icon.png')
+            .setImage('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/Cheesing101.png')
+            .setTimestamp()
+            message.channel.send(embed);
+        }
+    }
 
     //Custom Help command by using the second argument.
     if (message.channel.id === channelID) {
-    if(helpArgs[0] === 'ghostlobby') {
-        const embed = new Discord.MessageEmbed()
-        .setAuthor('Cheesing in Destiny 2')
-        .addFields(
-            { name: 'Example Ghost lobby in D1', value: '[Watch video](https://www.youtube.com/watch?v=3w6A_ctkhEo)', inline: true},
-            { name: 'Example Ghost lobby in D2', value: '[Watch video](https://www.youtube.com/watch?v=92-SDZ4SR5Q)', inline: true}
-        )
-        .setColor('#ffcc00')
-        .setThumbnail('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/Cheese_Icon.png')
-        .setImage('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/GhostLobbies.png')
-        .setTimestamp()
-        message.channel.send(embed);
-    }}
+        if(helpArgs[0] === 'GhostLobby') {
+            const embed = new Discord.MessageEmbed()
+            .setAuthor('Cheesing in Destiny 2')
+            .addFields(
+                { name: 'Example Ghost lobby in D1', value: '[Watch video](https://www.youtube.com/watch?v=3w6A_ctkhEo)'},
+                { name: 'Example Ghost lobby in D2', value: '[Watch video](https://www.youtube.com/watch?v=92-SDZ4SR5Q)'}
+            )
+            .setColor('#ffcc00')
+            .setThumbnail('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/Cheese_Icon.png')
+            .setImage('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/GhostLobbies.png')
+            .setTimestamp()
+            message.channel.send(embed);
+        }
+    }
+
+    //Custom Help command by using the second argument.
+    if (message.channel.id === channelID) {
+        if(helpArgs[0] === 'Trials') {
+            const embed = new Discord.MessageEmbed()
+            .setAuthor('Cheesing in Destiny 2')
+            .addFields(
+                { name: 'Example Ghost lobby in D1', value: '[Watch video](https://www.youtube.com/watch?v=3w6A_ctkhEo)'},
+                { name: 'Example Ghost lobby in D2', value: '[Watch video](https://www.youtube.com/watch?v=92-SDZ4SR5Q)'}
+            )
+            .setColor('#ffcc00')
+            .setThumbnail('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/Cheese_Icon.png')
+            .setImage('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/GhostLobbies.png')
+            .setTimestamp()
+            message.channel.send(embed);
+        }
+    }
+
+    //Custom Help command by using the second argument.
+    if (message.channel.id === channelID) {
+        if(helpArgs[0] === 'Discords') {
+            const embed = new Discord.MessageEmbed()
+            .setAuthor('Cheesing in Destiny 2')
+            .addFields(
+                { name: 'Example Ghost lobby in D1', value: '[Watch video](https://www.youtube.com/watch?v=3w6A_ctkhEo)'},
+                { name: 'Example Ghost lobby in D2', value: '[Watch video](https://www.youtube.com/watch?v=92-SDZ4SR5Q)'}
+            )
+            .setColor('#ffcc00')
+            .setThumbnail('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/Cheese_Icon.png')
+            .setImage('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/GhostLobbies.png')
+            .setTimestamp()
+            message.channel.send(embed);
+        }
+    }
+
+    //Custom Help command by using the second argument.
+    if (message.channel.id === channelID) {
+        if(helpArgs[0] === 'OOB') {
+            const embed = new Discord.MessageEmbed()
+            .setAuthor('Cheesing in Destiny 2')
+            .addFields(
+                { name: 'Example Ghost lobby in D1', value: '[Watch video](https://www.youtube.com/watch?v=3w6A_ctkhEo)'},
+                { name: 'Example Ghost lobby in D2', value: '[Watch video](https://www.youtube.com/watch?v=92-SDZ4SR5Q)'}
+            )
+            .setColor('#ffcc00')
+            .setThumbnail('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/Cheese_Icon.png')
+            .setImage('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/GhostLobbies.png')
+            .setTimestamp()
+            message.channel.send(embed);
+        }
+    }
 
     //Normal usage of (prefix)help without any args. (Shows all of the commands and you should set the commands yourself)
     if (message.channel.id === channelID) {
@@ -45,10 +98,10 @@ module.exports.run = async (client, message, args) => {
         .setThumbnail('https://destiny2.falcon-gaming.net/wp-content/uploads/cheesing/Cheese_Icon.png')
         .addFields(
             { name: '101', value: 'Learn about the cheesing culture in Destiny 2'},
-            { name: 'ghostlobby', value: 'Learn about ghost lobbies'},
-            { name: 'trials', value: 'WIP'},
-            { name: 'discords', value: 'WIP'},
-            { name: 'OOB', value: 'WIP'}
+            { name: 'GhostLobby', value: 'Learn about ghost lobbies'},
+            { name: 'Trials', value: 'Learn about how to do ghost lobbies in trials'},
+            { name: 'Discords', value: 'Learn about discord communities dedicated to cheesing.'},
+            { name: 'OOB', value: 'Learn about the practicve of going Out of Bounds.'}
         )
         .setTimestamp()
         message.channel.send(embed);}
@@ -57,7 +110,7 @@ module.exports.run = async (client, message, args) => {
 
 module.exports.config = {
     name: "cheese",
-    description: "Supports the following parameters (101, ghostlobby)",
+    description: "Supports the following parameters (101, GhostLobby)",
     usage: "!cheese || !cheese <param>",
     accessableby: "Members",
     aliases: []
